d8874560f5c3cedabb368796eaaa4203a23aef37
Danakwt <66743842+Danakwt@users.noreply.github.com>
2020-06-17 22:51:55 +0300

    done

---
+++ ios-CW2-p1.playground/Contents.swift
@@ -0,0 +1,16 @@
+
+let family = ["Dana", "Abdullah", "Mohammad", "Fahar"]
+
+var evens = [2, 4, 6, 8, 10, 12]
+
+var grades = [92.5, 90.8, 93.0]
+
+var trueFalse = [true, true, true, false]
+
+print(family[0], family[3])
+
+print(evens[0], evens[5])
+
+print(grades[0], grades[2])
+
+print(trueFalse[0], trueFalse[3])
---
+++ ios-cw2-p2.MyPlayground.playground/Contents.swift
@@ -0,0 +1,15 @@
+var fruits = ["mango", "orange", "potato", "peach"]
+ 
+ print(fruits[0], fruits[3])
+
+fruits.append("stawberry")
+print(fruits)
+
+fruits.remove(at:2)
+print(fruits)
+
+fruits += ["Banana", "Watermelon", "Pinapple" , "Kiwi"]
+ print(fruits)
+
+fruits.removeAll()
+ print(fruits)
